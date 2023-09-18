fecfa2fe7144e5a0f34b9a75f76b1187c6ed8cf9
檀轶步棋 <57583509+Verplitic@users.noreply.github.com>
2023-09-18 11:34:20 +0800

    impl: extract self to new folder

--- src/runner/decompress.rs
+++ src/runner/decompress.rs
@@ -1,23 +1,28 @@
+use crate::lib::progress;
+use flate2::read;
 use std::error::Error;
-use std::{fs, io};
 use std::fs::File;
 use std::io::Read;
 use std::path::{Component, Components, Path, PathBuf};
-use flate2::read;
+use std::{fs, io};
 use zip::ZipArchive;
-use crate::lib::progress;
+
+#[cfg(target_family = "windows")]
+static UPD_NAME: &str = "seal-updater.exe";
+#[cfg(target_family = "unix")]
+static UPD_NAME: &str = "seal-updater";
 
 pub fn decompress(path: impl AsRef<Path>, target: impl AsRef<Path>) -> Result<(), Box<dyn Error>> {
     if path.as_ref().as_os_str().is_empty() {
         Err("指向更新文件路径为空")?;
     }
     let file = File::open(path.as_ref())?;
-    let lower = path.as_ref()
+    let lower = path
+        .as_ref()
         .extension()
         .ok_or("无法取得压缩文件扩展名")?
         .to_ascii_lowercase();
-    let ext = lower.to_str()
-        .ok_or("无法将文件扩展名转换为 UTF-8 编码")?;
+    let ext = lower.to_str().ok_or("无法将文件扩展名转换为 UTF-8 编码")?;
     print!("正在解压…  ");
     match ext {
         "zip" => unzip(file, target.as_ref()),
@@ -25,13 +30,13 @@ pub fn decompress(path: impl AsRef<Path>, target: impl AsRef<Path>) -> Result<()
             let decoder = read::GzDecoder::new(file);
             let (total, show) = match get_tar_count(decoder) {
                 Ok(t) => (t, true),
-                Err(_) => (0, false)
+                Err(_) => (0, false),
             };
             let file = File::open(path.as_ref())?;
             let decoder = read::GzDecoder::new(file);
             untar(decoder, target.as_ref(), show, total)
-        },
-        _ => Err(format!("压缩文件具有未知扩展名 {}", ext))?
+        }
+        _ => Err(format!("压缩文件具有未知扩展名 {}", ext))?,
     }
 }
 
@@ -41,10 +46,15 @@ fn unzip(file: File, target: &Path) -> Result<(), Box<dyn Error>> {
     for i in 0..arc_len {
         let mut zip_file = archive.by_index(i)?;
         // TODO: Trust and skip name check?
-        let name = zip_file.enclosed_name()
+        let name = zip_file
+            .enclosed_name()
             .ok_or("文件名不安全，可能导致 zip slip")?;
-        let dest = target.join(name);
-        progress::print_progress(i+1, arc_len);
+        let dest = if name.to_string_lossy() != UPD_NAME {
+            target.join(name)
+        } else {
+            target.join("new_updater").join(name)
+        };
+        progress::print_progress(i + 1, arc_len);
         make_file(&mut zip_file, &dest)?;
     }
 
@@ -56,7 +66,12 @@ fn get_tar_count(reader: impl Read) -> Result<usize, Box<dyn Error>> {
     Ok(archive.entries()?.count())
 }
 
-fn untar(reader: impl Read, target: &Path, show_prog: bool, total: usize) -> Result<(), Box<dyn Error>> {
+fn untar(
+    reader: impl Read,
+    target: &Path,
+    show_prog: bool,
+    total: usize,
+) -> Result<(), Box<dyn Error>> {
     let is_path_safe = |com: Components| {
         let normals: Vec<Component> = com
             .into_iter()
@@ -73,9 +88,13 @@ fn untar(reader: impl Read, target: &Path, show_prog: bool, total: usize) -> Res
             // TODO: Trust and skip name check?
             Err("文件名不安全，可能导致 slip")?;
         }
-        let dest = target.join(name);
+        let dest = if name.to_string_lossy() != UPD_NAME {
+            target.join(name)
+        } else {
+            target.join("new_updater").join(name)
+        };
         if show_prog {
-            progress::print_progress(i+1, total);
+            progress::print_progress(i + 1, total);
         }
         make_file(&mut tar_file, &dest)?;
     }
